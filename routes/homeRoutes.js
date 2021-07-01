a4c819bad08510ed42599a10d493188f85184081
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2021-07-01 19:22:47 +0000

    created get route and was able to display post on the homepage

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,24 @@
 const express=require('express')
 const router= express.Router()
+const {Post, User}= require("../models")
 
-router.get("/", async (req,res)=>{
-    return await res.render('index')
-})
+// router.get("/", async (req,res)=>{
+//     return await res.render('index')
+// });
+
+router.get('/', async (req, res)=>{
+    try{
+        const posts= Post.findAll({
+            include:[User]
+        })
+
+        const postData= (await posts).map((post)=>post.get({plain:true}))
+        console.log(postData)
+        res.render('index', {postData})
+    } catch(err){
+        console.log(err)
+    }
+   
+});
 
 module.exports=router;
