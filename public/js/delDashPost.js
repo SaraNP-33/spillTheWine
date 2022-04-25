77f9c1bad60dd596d87fd675c09a7e158a0e7a67
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-04-25 19:47:51 +0000

    added put route and an even listener in public js to allow post to update

--- public/js/delDashPost.js
+++ public/js/delDashPost.js
@@ -1,6 +1,29 @@
 
 
 const delBtn= document.querySelector(".delPost")
+const upBtn =document.querySelector(".upPost")
+
+
+const updatePost= async (event)=>{
+    const upTitle= document.querySelector('#upTitle').value
+    const upBody =document.querySelector('#upBody').value
+
+    const id=event.target.getAttribute("data-id")
+    const response= await fetch(`/api/post/${id}`,{
+        method:"PUT",
+        body:JSON.stringify({
+            title:upTitle,
+            body:upBody
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    if (response.ok) {
+        document.location.replace('/dashboard');
+        alert("Your Post was sucessfully updated!")
+      } else {
+        alert('Failed to login');
+      }
+}
 
 const deletePost= async (event)=>{
     const id=event.target.getAttribute("data-id")
@@ -18,4 +41,5 @@ const deletePost= async (event)=>{
     }
 }
 
+upBtn.addEventListener("click", updatePost)
 delBtn.addEventListener("click", deletePost)
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -17,6 +17,23 @@ const withAuth = require("../../utils/auth")
       }
     })
 
+    router.put("/:id", withAuth, async (req,res)=>{
+      console.log(req.body, "from update")
+      try{
+        const upPost= await Post.update(req.body,{
+          where:{
+            id:req.params.id
+          }
+          
+        })
+        res.status(200).json(upPost)
+        console.log(upPost)
+      }catch(err){
+        res.status(400).json(err)
+      }
+    })
+
+
     router.delete("/:id", withAuth, async (req,res)=>{
         try{
           const delPost= await Post.destroy({
